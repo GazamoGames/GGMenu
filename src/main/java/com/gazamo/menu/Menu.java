d9de1008f532cec6b31babacf740c99bcddf1a1a
Mark Hendriks <mark.hendriks@castawaylabs.com>
2016-02-25 23:20:06 +0100

    Remove menu meta on close

--- src/main/java/com/gazamo/menu/Menu.java
+++ src/main/java/com/gazamo/menu/Menu.java
@@ -141,6 +141,7 @@ public abstract class Menu implements Listener, Container {
         if (this.hasOpen(player)) {
             this.components.keySet().forEach(component -> component.onOpen(player));
             this.getCloseConsumer().accept(player, this);
+            Menu.closeMenu(player);
         }
     }
 
@@ -249,6 +250,10 @@ public abstract class Menu implements Listener, Container {
                 .map(MetadataValue::value)
                 .filter(Menu.class::isInstance).map(Menu.class::cast)
                 .findFirst();
+    }
+
+    protected static void closeMenu(Player player) {
+        player.removeMetadata(MENU_KEY, getHolder());
     }
 
     protected static void setMenu(Player player, Menu menu) {
